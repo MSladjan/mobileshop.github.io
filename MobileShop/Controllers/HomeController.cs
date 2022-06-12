5c8b537c5a0160febb0b02ccfe532a14521925e6
Sladjan Mihajlovic <msladjan56@gmail.com>
2022-06-13 00:09:55 +0200

    Added new file

---
+++ MobileShop/Controllers/HomeController.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json;
+using MobileShop.DAL;
+using MobileShop.Models;
+using MobileShop.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MobileShop.Models.Home;
+
+namespace MobileShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbMobileShopSMEntities ctx = new dbMobileShopSMEntities();
+        public ActionResult Index(string search, int? page)
+        {
+            HomeIndexViewModel model = new HomeIndexViewModel();
+            return View(model.CreateModel(search, 8, page));
+        }
+
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        public ActionResult CheckoutDetails()
+        {
+            return View();
+        }
+
+        public ActionResult FinishOrder()
+        {
+            return View();
+        }
+
+        public ActionResult Samsung()
+        {
+            return View();
+        }
+
+        public ActionResult iPhone()
+        {
+            return View();
+        }
+
+        public ActionResult Huawei()
+        {
+            return View();
+        }
+
+        public ActionResult AddToCart(int productId)
+        {
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = ctx.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = ctx.Tbl_Product.Find(productId);
+                foreach (var item in cart)
+                {
+                    if (item.Product.ProductId == productId)
+                    {
+                        int prevQty = item.Quantity;
+                        cart.Remove(item);
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = prevQty + 1
+                        });
+                        break;
+                    }
+                    else
+                    {
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = 1
+                        });
+                    }
+                }
+                Session["cart"] = cart;
+            }
+            return Redirect("Index");
+        }
+        public ActionResult RemoveFromCart(int productId)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+            foreach (var item in cart)
+            {
+                if (item.Product.ProductId == productId)
+                {
+                    cart.Remove(item);
+                    break;
+                }
+            }
+            Session["cart"] = cart;
+            return Redirect("Index");
+        }
+    }
+}
+
